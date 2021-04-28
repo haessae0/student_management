2dcc79c6d7452faa9ed9c2788b655bdcf4338ac5
haessae0 <haessae0@gmail.com>
2021-04-28 09:17:50 +0900

    학생 추가와 같은 기능을 제공하기에 사용설명서로 변경

--- src/main/java/com/up/student/view/UpdateView.java
+++ src/main/java/com/up/student/view/UpdateView.java
@@ -15,13 +15,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import com.up.student.AppConstants;
 import com.up.student.DAO;
 import com.up.student.base.BaseDAO;
 import com.up.student.dao.StudentDAO;
 import com.up.student.model.Student;
+import com.up.student.view.MainView;
 
 /**
  * 模块说明： 更新学生信息
@@ -32,7 +36,7 @@ public class UpdateView extends JFrame {
 	private static final long serialVersionUID = 5292738820127102731L;
 
 	private JPanel jPanelCenter, jPanelSouth;
-	private JButton updateButton, exitButton;
+	private JButton searchButton, exitButton;
 	private JTextField name, sno, department, hometown, mark, email, tel, sex;
 
 	public UpdateView() {
@@ -40,42 +44,42 @@ public class UpdateView extends JFrame {
 	}
 
 	private void init() {
-		setTitle(AppConstants.UPDATEVIEW_TITLE);
+		setTitle(AppConstants.SEARCHVIEW_TITLE);
 		// center panel
 		jPanelCenter = new JPanel();
-		jPanelCenter.setLayout(new GridLayout(9, 2));
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_NAME));
-		name = new JTextField();
-		jPanelCenter.add(name);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_SNO));
-		sno = new JTextField();
-		jPanelCenter.add(sno);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_SEX));
-		sex = new JTextField();
-		jPanelCenter.add(sex);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_DEPARTMETN));
-		department = new JTextField();
-		jPanelCenter.add(department);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_HOMETOWN));
-		hometown = new JTextField();
-		jPanelCenter.add(hometown);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_MARK));
-		mark = new JTextField();
-		jPanelCenter.add(mark);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_EMAIL));
-		email = new JTextField();
-		jPanelCenter.add(email);
-		jPanelCenter.add(new JLabel(AppConstants.STUDENT_TEL));
-		tel = new JTextField();
-		jPanelCenter.add(tel);
-		jPanelCenter.add(new JLabel("-------------------------------------------------"));
-		jPanelCenter.add(new JLabel("-------------------------------------------------"));
+		jPanelCenter.setLayout(new GridLayout(9, 1));
+		jPanelCenter.add(new JLabel("----------------------------사용설명서----------------------------"));
+		//name = new JTextField();
+		//jPanelCenter.add(name);
+		jPanelCenter.add(new JLabel("1. 메인 화면의 좌상단 빈칸을 통해 학생의 이름을 입력시 관련 학생의 정보가 제공됩니다."));
+		//sno = new JTextField();
+		//jPanelCenter.add(sno);
+		jPanelCenter.add(new JLabel("검색 관련 예시 : 민 검색 -> 민 이라는 글자를 포함한 학생의 이름 모두 표시"));
+		//sex = new JTextField();
+		//jPanelCenter.add(sex);
+		jPanelCenter.add(new JLabel("하지만 빈칸, null, 데이터에 저장되지 않은 이름의 글자가 입력 될 경우 경고창이 표출됩니다."));
+		//department = new JTextField();
+		//jPanelCenter.add(department);
+		jPanelCenter.add(new JLabel("2. 학색 추가 : 학생의 이름, 학번, 성별, 전공, 주소, 학점, 이메일, 전화번호를 기입합니다."));
+		//hometown = new JTextField();
+		//jPanelCenter.add(hometown);
+		jPanelCenter.add(new JLabel("3. 학생 삭제 : 학생의 이름과 학번 입력시, 그에 해당하는 학생의 정보가 삭제 됩니다."));
+		//mark = new JTextField();
+		//jPanelCenter.add(mark);
+		jPanelCenter.add(new JLabel("4. 하단의 페이지 넘김 버튼을 통해 한 페이지 혹 맨 끝으로 이동하기의 기능이 제공됩니다."));
+		//email = new JTextField();
+		//jPanelCenter.add(email);
+		jPanelCenter.add(new JLabel("---------------------------------------------------------------"));
+		//tel = new JTextField();
+		//jPanelCenter.add(tel);
+		/*jPanelCenter.add(new JLabel("-------------------------------------------------"));
+		jPanelCenter.add(new JLabel("-------------------------------------------------"));*/
 
 		// south panel
 		jPanelSouth = new JPanel();
-		jPanelSouth.setLayout(new GridLayout(1, 2));
-		updateButton = new JButton(AppConstants.UPDATEVIEW_UPDATEBUTTON);
-		updateButton.addActionListener(new ActionListener() {
+		/*jPanelSouth.setLayout(new GridLayout(1, 2));
+		searchButton = new JButton(AppConstants.SEARCHVIEW_SEARCHBUTTON);
+		searchButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (check()) {
@@ -94,7 +98,7 @@ public class UpdateView extends JFrame {
 				}
 			}
 		});
-		jPanelSouth.add(updateButton);
+		jPanelSouth.add(searchButton);*/
 		exitButton = new JButton(AppConstants.EXITBUTTON);
 		exitButton.addActionListener(new ActionListener() {
 			@Override
@@ -108,12 +112,12 @@ public class UpdateView extends JFrame {
 		this.add(jPanelSouth, BorderLayout.SOUTH);
 
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setBounds(470, 200, 400, 270);
+		setBounds(470, 200, 510, 270);
 		setResizable(false);
 		setVisible(true);
 	}
 
-	private boolean check() {
+	/*private boolean check() {
 		boolean result = false;
 		if ("".equals(name.getText()) || "".equals(sno.getText()) || "".equals(department.getText())
 				|| "".equals(sex.getText()) || "".equals(mark.getText()) || "".equals(tel.getText())
@@ -145,5 +149,6 @@ public class UpdateView extends JFrame {
 		hometown.setText("");
 		tel.setText("");
 		mark.setText("");
-	}
+	}*/
+	
 }
